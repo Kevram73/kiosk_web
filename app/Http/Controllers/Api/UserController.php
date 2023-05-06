32d0546792da99907f971dfd8ea6f06a80825168
Dell <eyramativon1@gmail.com>
2023-05-07 00:01:25 +0200

    hh

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\API\BaseController as BaseController;
+use App\Http\Controllers\Api\BaseController as BaseController;
 
 class UserController extends BaseController
 {
