1b3e26399f827c64ab24aa1b7f32ee542e7cc6ed
Dell <eyramativon1@gmail.com>
2023-05-20 17:42:19 +0200

    datatables using

--- app/Http/Controllers/Api/ReglementController.php
+++ app/Http/Controllers/Api/ReglementController.php
@@ -16,7 +16,7 @@ class ReglementController extends BaseController
 
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:sanctum');
     }
 
     public function index(){
