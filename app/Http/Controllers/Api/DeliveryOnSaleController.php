b1e6e782f13abd154007aae07d1b29e6f807de0a
Dell <eyramativon1@gmail.com>
2023-05-27 12:47:42 +0200

    cool

--- app/Http/Controllers/Api/DeliveryOnSaleController.php
+++ app/Http/Controllers/Api/DeliveryOnSaleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Historique;
+use App\vente;
 use App\Livraisonvente;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
