4967b53380735661a83921dc5ba15cf36be1b14f
Dell <eyramativon1@gmail.com>
2023-05-28 03:21:45 +0200

    dd

--- app/Http/Controllers/Api/DeliveryOnSaleController.php
+++ app/Http/Controllers/Api/DeliveryOnSaleController.php
@@ -16,7 +16,7 @@ class DeliveryOnSaleController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:sanctum');
+        // $this->middleware('auth:sanctum');
     }
 
     public function index(Request $request)
