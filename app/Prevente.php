8a80bf114f8640f63ef1d515201e2d44803ce82b
Dell <eyramativon1@gmail.com>
2023-06-05 02:07:34 +0200

    prevente stuff

--- app/Prevente.php
+++ app/Prevente.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Prevente extends Model
 {
     public function modelefournisseur(){
-        return modeleFournisseur::find($this->modele_fournisseur_id);
+        return Modele::find($this->modele_fournisseur_id);
     }
 }
 
