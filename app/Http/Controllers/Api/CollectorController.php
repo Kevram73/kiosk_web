18bac5e3f46aaeaf14eec5fb764b1cfb51be0c91
Dell <eyramativon1@gmail.com>
2023-06-12 20:30:37 +0200

    Collectors

--- app/Http/Controllers/Api/CollectorController.php
+++ app/Http/Controllers/Api/CollectorController.php
@@ -14,6 +14,7 @@ class CollectorController extends BaseController
     public function get_list_collectors(Request $request){
 
         $users = User::all();
+        $collectors = [];
         foreach($users as $user){
             if($user->role() == "COLLECTOR"){
                 $collectors[] = $user;
