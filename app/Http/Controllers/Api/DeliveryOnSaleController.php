a0f593ae6739d2e7606638f4bc73adc8b53602f4
Kevram73 <eyramativon1@gmail.com>
2023-08-21 16:30:31 +0200

    kk

--- app/Http/Controllers/Api/DeliveryOnSaleController.php
+++ app/Http/Controllers/Api/DeliveryOnSaleController.php
@@ -22,7 +22,7 @@ class DeliveryOnSaleController extends Controller
 
     public function index(Request $request)
     {
-        $livraisons = LivraisonVenteS::where('boutique_id', Auth::user()->boutique()->id)->get()->take(10);
+        $livraisons = LivraisonVenteS::all()->take(10);
         return LivraisonVenteResource::collection($livraisons);
     }
 
