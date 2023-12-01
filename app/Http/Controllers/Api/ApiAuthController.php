78ed5e3ee5fbdd1786046adac05913927ed85fca
Kevram73 <eyramativon1@gmail.com>
2023-12-01 10:11:41 +0100

    cool

--- app/Http/Controllers/Api/ApiAuthController.php
+++ app/Http/Controllers/Api/ApiAuthController.php
@@ -31,7 +31,7 @@ class ApiAuthController extends BaseController
         }
 
         $token = $request->user()->createToken('API Token')->plainTextToken;
-        return response()->json(['token' => $token, 'user' => $request->user(), "role" => $request->user()->role()]);
+        return response()->json(['token' => $token, 'user' => $request->user(), "role" => $request->user()->getRolesName()]);
     }
 
 
@@ -71,7 +71,7 @@ class ApiAuthController extends BaseController
         }
 
         $token = $request->user()->createToken('API Token')->plainTextToken;
-        return response()->json(['token' => $token, 'user' => $request->user(), "role" => $request->user()->role()]);
+        return response()->json(['token' => $token, 'user' => $request->user(), "role" => $request->user()->getRoleNames()]);
     }
 
     public function register_collector(Request $request){
