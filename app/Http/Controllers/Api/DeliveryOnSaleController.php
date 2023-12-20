cce2a433a1faf23ab1856fe0137ce8761d9e8e86
Kevram73 <eyramativon1@gmail.com>
2023-12-20 23:07:56 +0100

    cool stuff

--- app/Http/Controllers/Api/DeliveryOnSaleController.php
+++ app/Http/Controllers/Api/DeliveryOnSaleController.php
@@ -52,8 +52,13 @@ class DeliveryOnSaleController extends BaseController
 
         // Filter the existing preventeIds based on the conditions
         $filteredPreventeIds = array_intersect($preventeIds, $livraisonventePreventeIds, array_diff($livraisonventePreventeIds, $preventeIdsNotInTable));
-        
-        return $filteredPreventeIds;
+        $ventesCorresponding = Vente::where('type_vente', 3)
+            ->whereHas('preventes', function ($query) use ($filteredPreventeIds) {
+                $query->whereIn('id', $filteredPreventeIds);
+            })
+            ->where('boutique_id', $boutiqueId)
+            ->get();
+        return SaleResource::collection($ventesCorresponding);
     }
 
     public function filter(Request $request){
